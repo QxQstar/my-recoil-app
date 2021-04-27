1e9f1a875398fb946f8d58f3e59ee58637c748bb
heyu <1395294694@qq.com>
2021-04-27 13:42:55 +0800

    feat: 修改

--- packages/math-he/src/add.js
+++ packages/math-he/src/add.js
@@ -1,3 +1,7 @@
 export function addNum (a,b) {
     return a + b;
 }
+
+export function addStr (a, b) {
+    return a + b;
+}
--- packages/request-he/index.js
+++ packages/request-he/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 import { addNum } from 'math-he';
 
-console.log(addNum(2,3))
+console.log(addNum(2,4))
 export default new axios();
