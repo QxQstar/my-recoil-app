7906e2e5078174c04bea75c63376a550171732d6
heyu <1395294694@qq.com>
2021-04-28 10:04:45 +0800

    feat: 增加 math 方法

--- packages/math-he/src/add.js
+++ packages/math-he/src/add.js
@@ -5,3 +5,9 @@ export function addNum (a,b) {
 export function addStr (a, b) {
     return a + b + '';
 }
+
+export function addArr(arr) {
+    return arr.reduce((a, b) => {
+        return a + b;
+    }, 0)
+}
