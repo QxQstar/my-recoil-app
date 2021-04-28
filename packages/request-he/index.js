ca3b2b2c6e6894cd20eddce28bcf8f27d28f4870
heyu <1395294694@qq.com>
2021-04-28 10:33:40 +0800

    feat: 修改 request

--- packages/request-he/index.js
+++ packages/request-he/index.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 import { addStr, addArr } from 'math-he';
 
 console.log(addStr(2,4))
-console.log(addArr([2,4]))
+console.log(addArr([2,4,5]))
 export default new axios();
